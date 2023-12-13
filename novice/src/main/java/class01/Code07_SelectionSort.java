05b37498ff585762bbf104ca78048e2a488ade04
叶明杰 <baihong.ymj@raycloud.com>
2023-12-13 22:28:53 +0800

    选择排序

---
+++ novice/src/main/java/class01/Code07_SelectionSort.java
@@ -0,0 +1,48 @@
+package class01;
+
+/**
+ * @author yemingjie.
+ * @date 2023/12/13.
+ * @time 22:15.
+ */
+public class Code07_SelectionSort {
+
+    public static void selectSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        for (int i = 0; i < N; i++) {
+            int minValueIndex = i;
+            for (int j = i + 1; j < N; j++) {
+                minValueIndex = arr[j] < arr[minValueIndex] ? j : minValueIndex;
+            }
+            swap(arr, i, minValueIndex);
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 1, 3, 5, 1, 6, 8, 1, 3, 5, 7, 5, 6};
+        printArray(arr);
+        selectSort(arr);
+        printArray(arr);
+
+    }
+
+
+}
