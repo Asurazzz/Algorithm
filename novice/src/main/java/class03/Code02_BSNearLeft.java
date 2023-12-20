a30411194a2994930409fc384f9771f3877dc291
叶明杰 <baihong.ymj@raycloud.com>
2023-12-20 22:45:28 +0800

    有序数组中找到 >= num 最左的位置

---
+++ novice/src/main/java/class03/Code02_BSNearLeft.java
@@ -0,0 +1,74 @@
+package class03;
+
+import java.util.Arrays;
+
+/**
+ * @author yemingjie.
+ * @date 2023/12/20.
+ * @time 22:31.
+ */
+public class Code02_BSNearLeft {
+
+    /**
+     * arr 有序， >= num 最左的位置
+     *
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static int mostLeftNoLessNum(int[] arr, int num) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int L = 0;
+        int R = arr.length - 1;
+        int ans = -1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (arr[mid] >= num) {
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return ans;
+
+    }
+
+    // for test
+    public static int test(int[] arr, int value) {
+       for (int i = 0; i < arr.length; i++) {
+           if (arr[i] >= value) {
+               return i;
+           }
+       }
+        return -1;
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 5000;
+        int maxSize = 10;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            Arrays.sort(arr);
+            int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            if (test(arr, value) != mostLeftNoLessNum(arr, value)) {
+                System.out.println("出错了！");
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+}
