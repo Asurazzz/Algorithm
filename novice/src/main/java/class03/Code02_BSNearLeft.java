1f76a1d0b6020dd76c196e4301e6a3551972e57f
叶明杰 <baihong.ymj@raycloud.com>
2023-12-20 23:18:40 +0800

    有序数组中找到 <= num 最右的位置

--- novice/src/main/java/class03/Code02_BSNearLeft.java
+++ novice/src/main/java/class03/Code02_BSNearLeft.java
@@ -36,13 +36,50 @@ public class Code02_BSNearLeft {
 
     }
 
+    /**
+     * arr 有序， <= num 最右的位置
+     *
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static int mostRightNoLessNum(int[] arr, int num) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int L = 0;
+        int R = arr.length - 1;
+        int ans = -1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (arr[mid] <= num) {
+                ans = mid;
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+        return ans;
+
+    }
+
+
+    public static int test1(int[] arr, int value) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] <= value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     // for test
     public static int test(int[] arr, int value) {
-       for (int i = 0; i < arr.length; i++) {
-           if (arr[i] >= value) {
-               return i;
-           }
-       }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= value) {
+                return i;
+            }
+        }
         return -1;
     }
 
@@ -63,12 +100,17 @@ public class Code02_BSNearLeft {
             int[] arr = generateRandomArray(maxSize, maxValue);
             Arrays.sort(arr);
             int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
-            if (test(arr, value) != mostLeftNoLessNum(arr, value)) {
+            if (test1(arr, value) != mostRightNoLessNum(arr, value)) {
+                System.out.println(Arrays.toString(arr));
+                System.out.println(value);
+                System.out.println(test1(arr, value));
+                System.out.println(mostRightNoLessNum(arr, value));
                 System.out.println("出错了！");
                 succeed = false;
                 break;
             }
         }
+
         System.out.println(succeed ? "Nice!" : "Fucking fucked!");
     }
 }
